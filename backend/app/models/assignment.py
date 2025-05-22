48bbc8c5d00273df7080fb815bbb2b9e236bccb2
Jaredan <Jaredanjerry@outlook.com>
2025-05-22 21:29:40 +0800

    Add assignment api.

--- backend/app/models/assignment.py
+++ backend/app/models/assignment.py
@@ -2,12 +2,12 @@
 from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.dialects.postgresql import ARRAY
 from app.db import Base
-
+import uuid
 
 class Assignment(Base):
     __tablename__ = "assignments"
 
-    assignment_id = Column(String, primary_key=True, index=True)
+    assignment_id = Column(String, primary_key=True, index=True, default=lambda: str(uuid.uuid4()))
     name = Column(String, index=True)
     course_id = Column(String, ForeignKey("courses.course_id"), index=True)
     teacher_id = Column(String, ForeignKey("users.user_id"), index=True)
--- backend/app/slides/assignment.py
+++ backend/app/slides/assignment.py
@@ -13,9 +13,9 @@ import json
 from fastapi import Form
 from app.auth.middleware import get_current_user
 from app.db import get_db
+from typing import List
 
 router = APIRouter()
-
 
 @router.get("/assignments/{course_id}")
 async def get_assignments(
@@ -49,12 +49,41 @@ async def get_assignments(
         ],
     }
 
-
 @router.post("/assignment")
 async def create_assignment(
+    name: str = Body(None),
+    is_group_assign: bool = Body(None),
+    course_id: str = Body(None),
+    deadline: str = Body(None),
+    files: List[str] = Body(None),
     db: Session = Depends(get_db),
     current_user: User = Depends(get_current_user),
-    is_group_assign: bool = Form(None),
-    course_id: str = Form(None),
 ):
-    pass
+    course = db.query(Course).filter(Course.course_id == course_id).first()
+    if not course:
+        return {"message": "Course not found"}
+
+    # Create new assignment
+    new_assignment = Assignment(
+        name=name,
+        course_id=course_id,
+        teacher_id=current_user.user_id,
+        deadline=deadline,
+        is_over=False,
+        is_group_assign=is_group_assign,
+        files=files
+    )
+    
+    # Add assignment to database
+    db.add(new_assignment)
+    db.commit()
+    db.refresh(new_assignment)
+
+    # Update course's assignments list
+    course.assignments.append(new_assignment.assignment_id)
+    db.commit()
+
+    return {
+        "assignment_id": new_assignment.assignment_id
+    }
+    
