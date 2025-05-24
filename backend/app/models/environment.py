8808f4409439df8409286d3de8563d0a2e15f7cd
Jaredan <Jaredanjerry@outlook.com>
2025-05-24 16:17:16 +0800

    fix: environment

--- backend/app/models/environment.py
+++ backend/app/models/environment.py
@@ -1,5 +1,5 @@
 from sqlalchemy import Column, String, ForeignKey, DateTime, Boolean
-from sqlalchemy.dialects.postgresql import ARRAY, UUID
+from sqlalchemy.dialects.postgresql import ARRAY, UUID, JSON
 from app.db import Base
 from datetime import datetime
 import uuid
@@ -17,5 +17,28 @@ class Environment(Base):
     wsUrl = Column(String, nullable=False)
     created_at = Column(DateTime, default=lambda: datetime.now())
     updated_at = Column(DateTime, default=lambda: datetime.now(), onupdate=lambda: datetime.now()) 
-    layout = Column(String, nullable=True, default='{"global":{"splitterEnableHandle":true,"tabSetEnableActiveIcon":true,"tabSetMinWidth":130,"tabSetMinHeight":100,"tabSetEnableTabScrollbar":true,"borderMinSize":100,"borderEnableTabScrollbar":true},"borders":[],"layout":{"type":"row","weight":100,"children":[{"type":"tabset","weight":100,"id":"main","children":[]}]}}')
     is_running = Column(Boolean, default=False)
+    layout = Column(JSON, nullable=True, default={
+        "global": {
+            "splitterEnableHandle": True,
+            "tabSetEnableActiveIcon": True,
+            "tabSetMinWidth": 130,
+            "tabSetMinHeight": 100,
+            "tabSetEnableTabScrollbar": True,
+            "borderMinSize": 100,
+            "borderEnableTabScrollbar": True
+        },
+        "borders": [],
+        "layout": {
+            "type": "row",
+            "weight": 100,
+            "children": [
+                {
+                    "type": "tabset",
+                    "weight": 100,
+                    "id": "main",
+                    "children": []
+                }
+            ]
+        }
+    })
