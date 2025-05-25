d086678dc2e4c59830266f226c4740c4e135ec07
Jaredan <Jaredanjerry@outlook.com>
2025-05-25 18:00:52 +0800

    term fix

--- backend/websocket/term.js
+++ backend/websocket/term.js
@@ -1,33 +1,25 @@
 import { WebSocketServer } from 'ws';
 import * as pty from "node-pty";
 
-const shell = "bash";
-const wss = new WebSocketServer({ host: '0.0.0.0', port: 8080 });
+const shell = "sh";
+const wss = new WebSocketServer({ host: '0.0.0.0', port: 4000 });
+
 let term = null;
 
-function nodeEnvBind() {
-    const term = pty.spawn(shell, ["--login"], {
+wss.on('connection', function connection(ws) {
+  if (!term) {
+    term = pty.spawn(shell, [], {
         name: "xterm-color",
-        cols: 80,
-        rows: 24,
-        cwd: "/root",
+        cwd: "/root/data",
         env: process.env,
     });
-    termMap.set(term.pid, term);
-    return term;
-}
-
-wss.on('connection', function connection(ws, req) {
-  if (!term) {
-    term = nodeEnvBind(req);
   }
 
-  term.on("data", function (data) {
+  term.onData((data) => {
     ws.send(data);
   });
 
   ws.on("message", (data) => {
-    console.log(typeof data === "string");
     term.write(data);
   });
 
